da380ac2d3ce4d2a4b46fcc2d9cb261065278891
Peterson <contact@peterson.by>
2018-09-18 16:16:56 +0300

    getZeroCount function implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+
+  let count = 0;
+  let fivePow = 5;
+  let pow = 2;
+  
+  while ((number / fivePow) > 1) {
+
+    count += Math.floor(number / fivePow);
+  	fivePow = Math.pow(5, pow);
+  	pow++;
+
+  }
+
+  return count;
 }
